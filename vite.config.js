53a7e04e4600ec0172ff1fe506dee9b046babcaa
miladmo68 <miladmo68@gmail.com>
2024-10-29 12:17:40 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/Gym-App/", // Ensure this matches your repository name
+});
