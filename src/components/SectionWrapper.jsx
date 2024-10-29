910002a52bdab976d3ba804503f23a8615966f0b
miladmo68 <miladmo68@gmail.com>
2024-10-29 12:22:43 -0400

    Initial commit

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function SectionWrapper(props) {
   const { children, header, title, id } = props;
   return (
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Gym-App/", // Ensure this matches your repository name
+  plugins: [react()],
+  base: "/Gym-App/",
 });
